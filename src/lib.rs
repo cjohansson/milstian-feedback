557ea67ee8a0221df558cea0bcde083847d148b5
Christian Johansson <christian@cvj.se>
2018-10-04 16:56:12 +0200

    Changed name from logger to feedback

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,34 @@
 extern crate chrono;
 use chrono::offset::Utc;
 
+//! # Milstian Feedback
+//! Used to handle logging of various types of messages.
+
 pub struct Logger;
 
 impl Logger {
+    /// # Output information
+    /// ```rust,dont_run
+    /// Logger::info("We have information".to_string());
+    /// ```
     pub fn info(message: String) {
         println!("{}", Logger::format_message(message));
     }
 
+    /// # Output error
+    /// ```rust,dont_run
+    /// Logger::error("We have a undefined error".to_string());
+    /// ```
     pub fn error(message: String) {
         eprintln!("{}", Logger::format_message(message));
     }
 
-    fn format_message(message: String) -> String {
+    /// # Format message with timestamp
+    /// ```rust
+    /// let message = Logger::format("Important stuff".to_string());
+    /// assert!(message.contains("stuff"));
+    /// ```
+    pub fn format_message(message: String) -> String {
         format!("{} - {}", Utc::now().format("%Y-%m-%d %H:%M:%S"), message)
     }
 }
